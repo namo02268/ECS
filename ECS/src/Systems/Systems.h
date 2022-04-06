1289940b1931f7c1f816e9671af2b964c9579922
namo02268 <namo02268@gmail.com>
2022-04-06 16:37:50 +0900

    [Update] namespace

--- ECS/src/Systems/Systems.h
+++ ECS/src/Systems/Systems.h
@@ -5,65 +5,67 @@
 #include "ECS/EntityHandle.h"
 #include "Components/Components.h"
 
-class TransformSystem : public System {
-public:
-	TransformSystem() {
-		auto family = getComponentTypeID<TransformComponent>();
-		m_requiredComponent[family] = true;
-		family = getComponentTypeID<RendererComponent>();
-		m_requiredComponent[family] = true;
-	}
-
-	void init() override {
-		for (auto& e : m_entityArray) {
-			auto tansformComponent = m_parentScene->getComponent<TransformComponent>(e);
-			std::cout << "id : " << e.GetID() << std::endl;
-			std::cout << "xpos : " << tansformComponent->xpos << std::endl;
-			std::cout << "ypos : " << tansformComponent->ypos << std::endl;
+namespace ECS {
+	class TransformSystem : public System {
+	public:
+		TransformSystem() {
+			auto family = getComponentTypeID<TransformComponent>();
+			m_requiredComponent[family] = true;
+			family = getComponentTypeID<RendererComponent>();
+			m_requiredComponent[family] = true;
 		}
-	}
 
-	void update(float dt) override {
+		void init() override {
+			for (auto& e : m_entityArray) {
+				auto tansformComponent = m_parentScene->getComponent<TransformComponent>(e);
+				std::cout << "id : " << e.GetID() << std::endl;
+				std::cout << "xpos : " << tansformComponent->xpos << std::endl;
+				std::cout << "ypos : " << tansformComponent->ypos << std::endl;
+			}
+		}
 
-	}
+		void update(float dt) override {
 
-	void draw() override {
+		}
 
-	}
-};
+		void draw() override {
 
-struct CollisionEvent : public Event {
-public:
-	CollisionEvent() { std::cout << "Called" << std::endl; }
-	~CollisionEvent() { std::cout << "Terminate" << std::endl; }
-};
+		}
+	};
 
-class CombatSystem : public System {
-public:
-	void init() override {
-		m_eventHandler->subscribe(this, &CombatSystem::onCollisionEvent);
-	}
-	void update(float dt) override {
+	struct CollisionEvent : public Event {
+	public:
+		CollisionEvent() { std::cout << "Called" << std::endl; }
+		~CollisionEvent() { std::cout << "Terminate" << std::endl; }
+	};
 
-	}
+	class CombatSystem : public System {
+	public:
+		void init() override {
+			m_eventHandler->subscribe(this, &CombatSystem::onCollisionEvent);
+		}
+		void update(float dt) override {
 
-	void draw() override {
+		}
 
-	}
-	void onCollisionEvent(CollisionEvent* collision) {
-		std::cout << "Recieved" << std::endl;
-	}
-};
+		void draw() override {
 
-class PhysicsSystem : public System {
-public:
-	void init() override {
-	}
-	void update(float dt) override {
-		CollisionEvent event;
-		m_eventHandler->publish(&event);
-	}
-	void draw() override {
+		}
+		void onCollisionEvent(CollisionEvent* collision) {
+			std::cout << "Recieved" << std::endl;
+		}
+	};
 
-	}
-};
+	class PhysicsSystem : public System {
+	public:
+		void init() override {
+		}
+		void update(float dt) override {
+			CollisionEvent event;
+			m_eventHandler->publish(&event);
+		}
+		void draw() override {
+
+		}
+	};
+}
