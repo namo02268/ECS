af9435192ffac90049e1be3df103096e06bc05bc
namo <namo02268@gmail.com>
2022-06-16 15:16:07 +0900

    [Test] array vs. unordered_map

--- ECS/src/ECS/EntityMap.h
+++ ECS/src/ECS/EntityMap.h
@@ -28,4 +28,28 @@ namespace ECS {
 			m_instanceToEntity[i] = e.GetID();
 		}
 	};
+	/*
+	class EntityMap {
+	private:
+		std::unordered_map<EntityID, ComponentInstance> m_entityToInstance;
+		std::array<EntityID, MAX_ENTITIES> m_instanceToEntity;
+
+	public:
+		~EntityMap() = default;
+
+		Entity getEntity(ComponentInstance i) { return m_instanceToEntity[i]; }
+		ComponentInstance getInstance(Entity e) { return m_entityToInstance[e.GetID()]; }
+
+		void add(Entity& e, ComponentInstance i) {
+			m_entityToInstance.insert({ e.GetID(), i });
+			m_instanceToEntity[i] = e.GetID();
+		}
+
+		void update(Entity& e, ComponentInstance i) {
+			m_entityToInstance[e.GetID()] = i;
+			m_instanceToEntity[i] = e.GetID();
+		}
+		void remove(Entity& e) { m_entityToInstance.erase(e.GetID()); }
+	};
+	*/
 }
