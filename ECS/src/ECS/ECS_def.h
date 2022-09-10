b2aef2b703603162f2031d29547471a09b74ef2e
namo <namo02268@gmail.com>
2022-09-10 10:50:05 +0900

    [Update] Defs

--- ECS/src/ECS/ECS_def.h
+++ ECS/src/ECS/ECS_def.h
@@ -3,13 +3,15 @@
 #include <bitset>
 #include <cstdint>
 
-using EntityID = std::int32_t;
-using ComponentTypeID = std::int32_t;
-using ComponentInstance = std::int32_t;
-using EventTypeID = std::int32_t;
+namespace ECS {
+	using EntityID = std::int32_t;
+	using ComponentTypeID = std::int32_t;
+	using ComponentInstance = std::int32_t;
+	using EventTypeID = std::int32_t;
 
-constexpr std::int32_t MAX_ENTITIES = 100000;
-constexpr std::int32_t MAX_COMPONENTS_FAMILY = 32;
-constexpr std::int32_t MAX_COMPONENTS = 100000;
+	constexpr std::int32_t MAX_ENTITIES = 100000;
+	constexpr std::int32_t MAX_COMPONENTS_FAMILY = 32;
+	constexpr std::int32_t MAX_COMPONENTS = 100000;
 
-using ComponentFamily = std::bitset<MAX_COMPONENTS_FAMILY>;
+	using ComponentFamily = std::bitset<MAX_COMPONENTS_FAMILY>;
+}
